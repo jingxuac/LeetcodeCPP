bccdad5063cce617efc74a37f9b5488fd33d9182
mac <JasonTsao@hahaha.local>
2015-10-24 19:59:32 -0400

    test1+1

--- LeetcodeCPP/TestCases.cpp
+++ LeetcodeCPP/TestCases.cpp
@@ -11,14 +11,14 @@
 void TestCase67::RunTestCase(){
     string a = "111";
     string b = "1000000";
-    cout<<"Input:"<<a<<","<<b<<"\nOutput:"<<Solution67::addBinary(a,b)<<"\n";
+    cout<<"\nInput:"<<a<<","<<b<<"\nOutput:"<<Solution67::addBinary(a,b)<<"\n";
     
     string c = "100";
     string d = "1000011";
-    cout<<"Input:"<<c<<","<<d<<"\nOutput:"<<Solution67::addBinary(c,d)<<"\n";
+    cout<<"\nInput:"<<c<<","<<d<<"\nOutput:"<<Solution67::addBinary(c,d)<<"\n";
     
-    string e = "0";
-    string f = "100";
-    cout<<"Input:"<<e<<","<<f<<"\nOutput:"<<Solution67::addBinary(e,f)<<"\n";
+    string e = "1";
+    string f = "1";
+    cout<<"\nInput:"<<e<<","<<f<<"\nOutput:"<<Solution67::addBinary(e,f)<<"\n";
     
 }
